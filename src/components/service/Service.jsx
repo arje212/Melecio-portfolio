e60214df4ed7f57f59f1dbfacb423f189546fdd0
arje212 <114086629+arje212@users.noreply.github.com>
2023-07-04 19:39:15 +0800

    Update Service.jsx

--- src/components/service/Service.jsx
+++ src/components/service/Service.jsx
@@ -9,7 +9,7 @@ const Service = () => {
     <div className='service__head'>
       <article className='service'>
         <div className='service__head'>
-          <h3>UI/UX Design</h3>
+          <h3>fsdf</h3>
         </div>
 
         <ul className='service__list'>
